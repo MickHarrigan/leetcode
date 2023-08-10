9747f7f83192ff53a4e5075d7420bd5a85bbe018
Mick Harrigan <mharrigan328@gmail.com>
2023-08-10 19:41:36 -0400

    33 complete, but it took a minute

--- src/33/src/main.rs
+++ src/33/src/main.rs
@@ -3,15 +3,46 @@ struct Solution;
 impl Solution {
     #[cfg(not(feature = "rusty"))]
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        return 0;
+        if nums.len() == 0 {
+            return -1;
+        }
+
+        let mut low = 0;
+        let mut high = nums.len() - 1;
+
+        while low <= high {
+            let mid = (high + low) / 2;
+            if nums[mid] == target {
+                return mid as i32;
+            }
+
+            if nums[low] <= nums[mid] {
+                if nums[low] <= target && nums[mid] > target {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if nums[mid] < target && nums[high] >= target {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
     #[cfg(feature = "rusty")]
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        return 0;
+        todo!();
     }
 }
 
-fn main() {}
+fn main() {
+    let sol = Solution::search(vec![5,1,3], 3);
+    println!("solution here is {}", sol);
+}
 
 #[cfg(test)]
 mod test;
--- src/33/src/test.rs
+++ src/33/src/test.rs
@@ -2,5 +2,39 @@ use super::*;
 
 #[test]
 fn empty_vec() {
-    assert_eq!(0,0);
+    assert_eq!(Solution::search(vec![], 0), -1);
 }
+
+#[test]
+fn single_item() {
+    assert_eq!(Solution::search(vec![1], 1), 0);
+}
+
+#[test]
+fn single_item_wrong() {
+    // this is also case 3
+    assert_eq!(Solution::search(vec![0], 1), -1);
+}
+
+#[test]
+fn case1() {
+    let mut vector = (0..=7).collect::<Vec<i32>>();
+    vector.remove(3);
+    vector.rotate_right(4);
+    println!("{:?}", vector);
+    assert_eq!(Solution::search(vector, 0), 4);
+}
+
+#[test]
+fn case2() {
+    let mut vector = (0..=7).collect::<Vec<i32>>();
+    vector.remove(3);
+    vector.rotate_right(4);
+    assert_eq!(Solution::search(vector, 3), -1);
+}
+
+#[test]
+fn unsorted_short() {
+    assert_eq!(Solution::search(vec![5,1,3], 3), 2);
+}
+
