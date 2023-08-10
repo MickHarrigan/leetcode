8d9238ed6cdaddf74b7e926f428ce49363b7268a
Mick Harrigan <mharrigan328@gmail.com>
2023-08-10 12:10:42 -0400

    Update the main readme and add in some features for more rusty stuff

--- src/33/src/main.rs
+++ src/33/src/main.rs
@@ -1,13 +1,17 @@
 struct Solution;
 
 impl Solution {
+    #[cfg(not(feature = "rusty"))]
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        return 0;
+    }
+    #[cfg(feature = "rusty")]
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
         return 0;
     }
 }
 
 fn main() {}
-
 
 #[cfg(test)]
 mod test;
