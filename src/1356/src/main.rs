18fa94dcaf0fc9ece36f6ecaf172a5d1bb4d97ad
Mick Harrigan <mharrigan328@gmail.com>
2023-10-30 11:23:56 -0700

    This change removes the match and converts it into a `.then()` call which I just learned about and I love it

--- src/1356/src/main.rs
+++ src/1356/src/main.rs
@@ -2,10 +2,7 @@ struct Solution;
 
 impl Solution {
     pub fn sort_by_bits(mut arr: Vec<i32>) -> Vec<i32> {
-        arr.sort_by(|a, b| match a.count_ones().cmp(&b.count_ones()) {
-            std::cmp::Ordering::Equal => a.cmp(&b),
-            other => other,
-        });
+        arr.sort_by(|a, b| a.count_ones().cmp(&b.count_ones()).then(a.cmp(b)));
         arr
     }
 }
@@ -16,4 +13,3 @@ fn main() {
     let sol = Solution::sort_by_bits(vec![1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]);
     println!("{:?}", sol);
 }
-
